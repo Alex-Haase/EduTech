cfa539ef8ff9db39484093ad8a110c50c4609fbe
Alex-Haase <137220834+Alex-Haase@users.noreply.github.com>
2025-05-20 12:06:48 -0400

    Add files via upload
    inicio de proyecto

---
+++ EduTech-vm/EduTech-vm/src/main/java/com/EduTech_vm/cl/EduTech_vm/EduTechVmApplication.java
@@ -0,0 +1,13 @@
+package com.EduTech_vm.cl.EduTech_vm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduTechVmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduTechVmApplication.class, args);
+	}
+
+}
