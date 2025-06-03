23a986a768e376c014fe5f5a0b5b976aaff6a85d
Pablocus <pa.mendezc@duocuc.cl>
2025-06-03 12:37:27 -0400

    Relleno Carrito Controller parte 1
    Se agrega el Agregar Curso al Carrito Controller

---
+++ EduTech-vm/src/main/java/com/EduTech_vm/cl/EduTech_vm/Controller/carritoController.java
@@ -0,0 +1,33 @@
+package com.EduTech_vm.cl.EduTech_vm.Controller;
+
+import com.EduTech_vm.cl.EduTech_vm.Model.Curso;
+import com.EduTech_vm.cl.EduTech_vm.Service.cursoService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/api/v1/carrito")
+public class carritoController {
+    private final List<Curso> carrito = new ArrayList<>();
+
+    @Autowired
+    private cursoService cursoService;
+    @PostMapping("/agregar/{id}")
+    public String agregarCurso(@RequestBody int id) {
+        Curso curso = cursoService.getCursoId(id);
+        if (curso != null) {
+            carrito.add(curso);
+            return "Curso agregado al carrito: " + curso.getTitulo();
+        }
+        return "Curso no encontrado";
+    }
+    
+}
