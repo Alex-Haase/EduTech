837e27f8d132cfe0f4dc63941e1453012e9c48f7
Pablocus <137220836+Pablocus@users.noreply.github.com>
2025-06-12 14:42:39 -0400

    Imports para Controlador Test de Mensajes

--- EduTech-vm/src/test/java/com/EduTech_vm/cl/EduTech_vm/Controller/MensajeControllerIntegrationTest.java
+++ EduTech-vm/src/test/java/com/EduTech_vm/cl/EduTech_vm/Controller/MensajeControllerIntegrationTest.java
@@ -1,5 +1,24 @@
 package com.EduTech_vm.cl.EduTech_vm.Controller;
 
+import com.EduTech_vm.cl.EduTech_vm.Model.MensajeContacto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.MediaType;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@WebMvcTest(MensajeController.class)
 public class MensajeControllerIntegrationTest {
     
 }
